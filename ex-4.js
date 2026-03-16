a3fe9e42d484727119c0504f60c3521d98835470
Chindurex <chinnaracha@gmail.com>
2026-03-16 16:11:26 +0700

    add answer ex1-6

--- ex-4.js
+++ ex-4.js
@@ -1,2 +1,27 @@
 // Start coding here
-let calculator = {};
+let add = (a,b) => {
+    return a+b;
+};
+
+let subtract = (a,b) => {
+    return a-b;
+};
+
+let multiply = (a,b) => {
+    return a*b;
+}
+
+let divide = (a,b) => {
+    return a/b;
+};
+
+let calculator = {add: add,
+                  subtract: subtract,
+                  multiply: multiply,
+                  divide: divide,
+};
+let addResult = (add(10,20));
+console.log(addResult);
+
+let divideResult = (divide(3000,10))
+console.log(divideResult)
--- ex-5.js
+++ ex-5.js
@@ -1,4 +1,15 @@
 // Start coding here
-const minNumber = findMinNumber(10, 20);
+// const minNumber = findMinNumber(10, 20);
 
-console.log(minNumber); // 10
+// console.log(minNumber); // 10
+
+
+function findMinNumber (a,b) {
+    return Math.min(a,b);
+};
+console.log(findMinNumber(10,5));
+
+// Math.min()
+// Math.min(value1)
+// Math.min(value1, value2)
+// Math.min(value1, value2, /* …, */ valueN)
